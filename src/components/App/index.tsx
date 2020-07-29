8f404c6b9397e9328206b8d762cb3a8000b7ccfb
ryguy806 <L1ghtSab3r11!!>
2020-07-29 09:25:10 -0700

    Created border, and header display.

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+import './App.scss';
+import NumberDisplay from '../NumberDisplay';
+
+const App: FC = () => {
+    return (
+        <div className="App">
+            <div className="Header">
+            <NumberDisplay value={0}/>
+            <div className="Face"> <span role="img" aria-label="face">😃</span></div>
+            <NumberDisplay value={23}/>
+            </div>
+            <div className="Body">Body</div>
+        </div>
+    )
+}
+
+export default App;
